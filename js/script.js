2655aa42db7cbaced1abae675dbc91d08c04d5c8
SAM_1 <lams62ua@gmail.com>
2022-12-28 15:12:51 +0200

    WIN_js_Practice_015done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 "use script"; 
 
-// VAR's
-
 let numberOfFilms = +prompt('How many films do you whatch?', ''); 
 
 const personalMovieDB = {
@@ -11,15 +9,42 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('The last film you whatch 1?', ''), 
-      b = prompt('How can you rate it 1?', ''), 
-      c = prompt('The last film you whatch 2', ''), 
-      d = prompt('How can you rate it 2?', ''); 
+// const a = prompt('The last film you whatch 1?', ''), 
+//       b = prompt('How can you rate it 1?', ''), 
+//       c = prompt('The last film you whatch 2', ''), 
+//       d = prompt('How can you rate it 2?', ''); 
 
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d; 
+// personalMovieDB.movies[a] = b; 
+// personalMovieDB.movies[c] = d; 
 
+// console.log(personalMovieDB); 
+
+// ============ Practice 2 ============ // 
+
+for (let i = 0; i < 2; i++) {
+    
+    const a = prompt('The last film you whatch 1/2?', ''), 
+          b = prompt('How can you rate it 1/2?', ''); 
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b; 
+        console.log('Done!');
+    } else {
+        console.log('ERROR!');
+        i--; 
+    }
+}
 console.log(personalMovieDB); 
+
+if (personalMovieDB.count < 10) {
+    console.log('Little movie directory'); 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Middle movie directory'); 
+} else if (personalMovieDB.count >= 30) {
+    console.log('BIG movie directory'); 
+} else {
+    console.log('ERROR!'); 
+}
 
 
 
