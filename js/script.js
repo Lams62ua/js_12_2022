ccabf330628d61e902b56c8d24226e9a39e3a71e
SAM_1 <lams62ua@gmail.com>
2022-12-30 13:11:39 +0200

    WIN_js_Practice_018done

--- js/script.js
+++ js/script.js
@@ -1,52 +1,83 @@
 "use script"; 
 
-let numberOfFilms = +prompt('How many films do you whatch?', ''); 
+// ============ Practice 3 (018) ============ // 
 
-const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {}, 
-    genres: [], 
-    privat: false,
-};
+let numberOfFilms; 
 
-// const a = prompt('The last film you whatch 1?', ''), 
-//       b = prompt('How can you rate it 1?', ''), 
-//       c = prompt('The last film you whatch 2', ''), 
-//       d = prompt('How can you rate it 2?', ''); 
+function start() {
+    numberOfFilms = +prompt('How many films do you whatch?', ''); 
 
-// personalMovieDB.movies[a] = b; 
-// personalMovieDB.movies[c] = d; 
-
-// console.log(personalMovieDB); 
-
-// ============ Practice 2 ============ // 
-
-for (let i = 0; i < 2; i++) {
-    
-    const a = prompt('The last film you whatch 1/2?', ''), 
-          b = prompt('How can you rate it 1/2?', ''); 
-
-    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-        personalMovieDB.movies[a] = b; 
-        console.log('Done!');
-    } else {
-        console.log('ERROR!');
-        i--; 
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films do you whatch?', ''); 
     }
 }
-console.log(personalMovieDB); 
 
-if (personalMovieDB.count < 10) {
-    console.log('Little movie directory'); 
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Middle movie directory'); 
-} else if (personalMovieDB.count >= 30) {
-    console.log('BIG movie directory'); 
-} else {
-    console.log('ERROR!'); 
+start(); 
+
+const personalMovieDB = {
+      count: numberOfFilms, 
+      movies: {}, 
+      genres: [], 
+      privat: true, 
+}; 
+
+function rememberMyFilm() {
+    for (let i = 0; i < 2; i++) {
+    
+        const a = prompt('The last film you whatch 1/2?', ''), 
+              b = prompt('How can you rate it 1/2?', ''); 
+    
+        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b; 
+            console.log('Done!');
+        } else {
+            console.log('ERROR!');
+            i--; 
+        }
+    }
+    console.log(personalMovieDB.movies); 
 }
 
+rememberMyFilm(); 
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Little movie directory'); 
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Middle movie directory'); 
+    } else if (personalMovieDB.count >= 30) {
+        console.log('BIG movie directory'); 
+    } else {
+        console.log('ERROR!'); 
+    }
+}
+
+detectPersonalLevel(); 
+
+function showMyDB(privatIsTrue) {
+    if (!privatIsTrue) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('This information is privat'); 
+    }
+}
+
+showMyDB(personalMovieDB.privat); 
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        const genre = prompt(`Your favorite Genre ${i+1}`);
+        if (genre !== '' && genre !== null) {
+            personalMovieDB.genres[i] = genre; 
+        } else {
+            i--;
+        }
+    }
+
+    console.log(personalMovieDB.genres); 
+}
+
+writeYourGenres(); 
 
 
-
-
+// ============ Practice 3 (018) GIT Push ============ // 
