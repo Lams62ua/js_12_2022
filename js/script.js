1d19d2bb4fdc6f341733b87242501ad412ad2817
SAM_1 <lams62ua@gmail.com>
2023-01-04 16:53:23 +0200

    WIN_js_Practice_024done

--- js/script.js
+++ js/script.js
@@ -1,83 +1,95 @@
 "use script"; 
 
-// ============ Practice 3 (018) ============ // 
+// ============ Practice 4 (024) ============ // 
 
-let numberOfFilms; 
-
-function start() {
-    numberOfFilms = +prompt('How many films do you whatch?', ''); 
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many films do you whatch?', ''); 
-    }
-}
-
-start(); 
 
 const personalMovieDB = {
-      count: numberOfFilms, 
-      movies: {}, 
-      genres: [], 
-      privat: true, 
+    count: 0, 
+    movies: {}, 
+    genres: [], 
+    privat: true, 
+    start: function() {
+        personalMovieDB.count = +prompt('How many films do you whatch?', ''); 
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How many films do you whatch?', ''); 
+        } 
+    }, 
+    rememberMyFilm: function() {
+        for (let i = 0; i < 2; i++) {
+        
+            const a = prompt('The last film you whatch 1/2?', ''), 
+                  b = prompt('How can you rate it 1/2?', ''); 
+        
+            if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+                personalMovieDB.movies[a] = b; 
+                console.log('Done!');
+            } else {
+                console.log('ERROR!');
+                i--; 
+            }
+        }
+        console.log(personalMovieDB.movies); 
+    }, 
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Little movie directory'); 
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Middle movie directory'); 
+        } else if (personalMovieDB.count >= 30) {
+            console.log('BIG movie directory'); 
+        } else {
+            console.log('ERROR!'); 
+        }
+    }, 
+    showMyDB: function(privatIsTrue) {
+        if (!privatIsTrue) {
+            console.log(personalMovieDB);
+        } else {
+            console.log('This information is privat'); 
+        }
+    }, 
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }, 
+    // ================ METHOD 1 =======================================================     
+    // writeYourGenres: function() {
+    //     for (let i = 0; i < 3; i++) {
+    //         const genre = prompt(`Your favorite Genre ${i+1}`);
+    //         if (genre !== '' && genre !== null) {
+    //             personalMovieDB.genres[i] = genre; 
+    //         } else {
+    //             console.log('Incorrect text!'); 
+    //             i--;
+    //         }
+    //     }
+    //     personalMovieDB.genres.forEach(function(item, num) {
+    //         console.log(`Favorite genre number ${num+1}, is - ${item}`); 
+    //     }); 
+    // }, 
+    // ================ METHOD 2 =======================================================     
+    writeYourGenres: function() {
+        for (let i = 0; i < 1; i++) {
+            const genres = prompt(`Write your three favorite GenreS (using , )`).toLowerCase(); 
+                if (genres !== '' && genres !== null) {
+                    personalMovieDB.genres = genres.split(", "); 
+                    personalMovieDB.genres.sort(); 
+                } else {
+                    console.log('Incorrect text!'); 
+                    i--;
+                }
+        }
+        personalMovieDB.genres.forEach(function(item, num) {
+            console.log(`Your favorite genre number ${num+1}, is - ${item}`); 
+        }); 
+        console.log(personalMovieDB.genres); 
+    }, 
+
 }; 
 
-function rememberMyFilm() {
-    for (let i = 0; i < 2; i++) {
-    
-        const a = prompt('The last film you whatch 1/2?', ''), 
-              b = prompt('How can you rate it 1/2?', ''); 
-    
-        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-            personalMovieDB.movies[a] = b; 
-            console.log('Done!');
-        } else {
-            console.log('ERROR!');
-            i--; 
-        }
-    }
-    console.log(personalMovieDB.movies); 
-}
+// ============ Practice 4 (024) GIT Pushed ============ // 
 
-rememberMyFilm(); 
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Little movie directory'); 
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Middle movie directory'); 
-    } else if (personalMovieDB.count >= 30) {
-        console.log('BIG movie directory'); 
-    } else {
-        console.log('ERROR!'); 
-    }
-}
-
-detectPersonalLevel(); 
-
-function showMyDB(privatIsTrue) {
-    if (!privatIsTrue) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('This information is privat'); 
-    }
-}
-
-showMyDB(personalMovieDB.privat); 
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        const genre = prompt(`Your favorite Genre ${i+1}`);
-        if (genre !== '' && genre !== null) {
-            personalMovieDB.genres[i] = genre; 
-        } else {
-            i--;
-        }
-    }
-
-    console.log(personalMovieDB.genres); 
-}
-
-writeYourGenres(); 
-
-
-// ============ Practice 3 (018) GIT Push ============ // 
